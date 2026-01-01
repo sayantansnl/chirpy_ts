19ff3e0bf4b7cc5dff40ad591e96e06de97091b6
sayantansnl <sayantansanyal72@gmail.com>
2026-01-01 05:56:21 +0000

    static middleware used

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,7 @@
 import express from "express";
 const app = express();
-const port = 8080;
-app.listen(port);
+const PORT = 8080;
+app.use(express.static("."));
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
